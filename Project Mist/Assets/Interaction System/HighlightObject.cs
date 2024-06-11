333159c55bd8426e93efaee79ae983cb6a01569f
Bruno Wu <bruno7405@gmail.com>
2024-06-11 00:52:40 -0700

    Object Selection and Highlighting
    - Interactable objects will be highlighted when player looks at it at a certain distance

---
+++ Project Mist/Assets/Interaction System/HighlightObject.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class HighlightObject : MonoBehaviour
+{
+    public Color highlightedColor = Color.white;
+    static float intensity = 0.25f;
+
+    [SerializeField] private List<Renderer> renderers;
+    List<Material> materials;
+
+    void Start()
+    {
+        materials = new List<Material>();
+        foreach (var renderer in renderers)
+        {
+            //A single child-object might have mutliple materials on it
+            //that is why we need to all materials with "s"
+            materials.AddRange(new List<Material>(renderer.materials));
+        }
+    }
+
+    public void Highlight()
+    {
+        foreach (var material in materials)
+        {
+            //We need to enable the EMISSION
+            material.EnableKeyword("_EMISSION");
+            //before we can set the color
+            material.SetColor("_EmissionColor", highlightedColor * intensity);
+        }
+    }
+
+    public void DeHighlight()
+    {
+        foreach(var material in materials)
+        {
+            material.DisableKeyword("_EMISSION");
+        }
+    }
+}
---
+++ Project Mist/Assets/Interaction System/SelectionManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string interactableTag = "Interactable";
+
+    public float interactionDistance;
+    private Transform currentSelection;
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        // Shoot Raycast
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+
+            // Raycast Hit but it is a different object from current
+            if (selection != currentSelection && currentSelection != null) DeselectCurrent();
+
+            // Interaction Distance Checks
+            if (Vector3.Distance(selection.position, transform.position) > interactionDistance) return;
+
+            // Check if object is interactable
+            if (selection.CompareTag(interactableTag))
+            {
+                // Highlight object
+                var selectionRenderer = selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+                    HighlightObject highlightObject = selection.GetComponent<HighlightObject>();
+                    highlightObject?.Invoke(nameof(highlightObject.Highlight), 0);
+                }
+                else Debug.LogError("No Renderer on selected object!");
+
+                currentSelection = selection;
+            }
+        }
+
+        // Raycast did not hit & an item is selected
+        else if (currentSelection != null)
+        {
+            DeselectCurrent();
+        }
+    }
+
+    private void DeselectCurrent()
+    {
+        HighlightObject highlightObject = currentSelection.GetComponent<HighlightObject>();
+
+        highlightObject?.Invoke(nameof(highlightObject.DeHighlight), 0);
+
+        currentSelection = null;
+    }
+}
